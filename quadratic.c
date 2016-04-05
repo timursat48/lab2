ed55b9df37b5e5a19cf1522eba85e8cfaaabbbca
timursat48 <sattimur17@gmail.com>
2016-04-05 10:57:11 +0600

    3 part - Finding x1 x2

--- quadratic.c
+++ quadratic.c
@@ -11,4 +11,19 @@ int quadratic_equation (int a, int b, int c)
         d = pow(b, 2) - 4 * a * c;
 	printf ("D = %d\n",d);
         if (d < 0) printf("\a\n The equation has no real roots !");
+	else if (d == 0)
+        {
+            x1 = -b / (2 / a);
+            if (b == 0 && c == 0) x1 = 0;
+            printf(" The equation has one root: x = %d", x1);
+        }
+        else
+        {
+            x1 = (-b - sqrt(d)) / (2 * a);
+            x2 = (-b + sqrt(d)) / (2 * a);
+            printf (" The equation has two root: \n x1 = %d\n x2 = %d" , x1, x2);
+        }
+    }
+
+}
 }
