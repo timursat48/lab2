e0672b18f02b6d360853b9ae7ede1b0fb51296bc
timursat48 <sattimur17@gmail.com>
2016-04-05 10:32:21 +0600

    2 part - Finding discriminant

--- quadratic.c
+++ quadratic.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <math.h>
 
-int quadratic_equation (double a, double b, double c)
+int quadratic_equation (int a, int b, int c)
 {
+    int  d, x1 = 0, x2 = 0;
 
+    if (a == 0) printf("\a\n the equation has no roots ");
+    else
+    {
+        d = pow(b, 2) - 4 * a * c;
+	printf ("D = %d\n",d);
+        if (d < 0) printf("\a\n The equation has no real roots !");
 }
